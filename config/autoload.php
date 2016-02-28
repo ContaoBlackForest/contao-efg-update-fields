9bd4ea670df25257ec1c38c0e36707223ff04740
Sven Baumann <baumann.sv@gmail.com>
2016-02-28 14:42:34 +0100

    register autoload

---
+++ config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Copyright (C) contao-efg-update-fields
+ *
+ * @package   contao-efg-update-fields
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2016 ContaoBlackForest
+ */
+
+ClassLoader::addClasses(
+    array(
+        'ContaoBlackForest\EFG\UpdateFields\DataContainer\Controller' => 'system/modules/efg-update-fields/src/Controller.php',
+    )
+);
