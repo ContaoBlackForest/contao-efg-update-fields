6da5bd18ce914577f9663dc0459b68cc3d80a9dc
Sven Baumann <baumann.sv@gmail.com>
2016-03-21 12:46:29 +0100

    remove create, this don´t used any more after implement the save handler

--- src/Controller.php
+++ src/Controller.php
@@ -32,25 +32,7 @@ class Controller
             return;
         }
 
-        $this->handleByCreate();
         $this->handleBySave();
-    }
-
-    protected function handleByCreate()
-    {
-        if (\Input::get('act') !== 'create') {
-            return;
-        }
-
-        $formulaModel = \FormModel::findByPk(\Input::get('pid'));
-        if (!$formulaModel->storeFormdata) {
-            return;
-        }
-
-        $GLOBALS['TL_HOOKS']['outputBackendTemplate'][] = array(
-            'ContaoBlackForest\EFG\UpdateFields\DataContainer\Controller',
-            'updateFormDataDCAByCreate'
-        );
     }
 
     protected function handleBySave()
@@ -68,26 +50,6 @@ class Controller
             'ContaoBlackForest\EFG\UpdateFields\DataContainer\Controller',
             'updateFormDataDCABySave'
         );
-    }
-
-    /**
-     * @param $buffer
-     * @param $template
-     *
-     * @return mixed
-     */
-    public function updateFormDataDCAByCreate($buffer, $template)
-    {
-        $formFieldId = \Input::get('id');
-        \Input::setGet('id', \Input::get('pid'));
-        $formTable = new \DC_Table('tl_form');
-
-        $formDataBackend = new FormdataBackend();
-        $formDataBackend->createFormdataDca($formTable);
-
-        \Input::setGet('id', $formFieldId);
-
-        return $buffer;
     }
 
     /**
